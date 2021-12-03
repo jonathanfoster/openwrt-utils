53c6856877185277ab56d0bd876b0732d2a25794
Jonathan Foster <jonathan@jonathanfoster.io>
2021-12-03 00:15:12 -0500

    Parse backup command args

--- src/backup-s3.sh
+++ src/backup-s3.sh
@@ -2,38 +2,71 @@
 # shellcheck shell=dash
 set -e
 
-if [ -n "$AWS_ACCESS_KEY_ID" ]; then
-  echo "AWS_ACCESS_KEY_ID not set."
+command=$(basename "$0")
+
+usage() {
+  echo "usage: $command -b <aws-backup-bucket> -n <hostname> [-k <aws-access-key-id>] [-s <aws-secret-access-key>] [-r <aws-region>]"
+}
+
+while getopts "b:n:k:s:r:h" flag
+do
+    case "${flag}" in
+        b) aws_backup_bucket=${OPTARG} ;;
+        n) hostname=${OPTARG} ;;
+        k) aws_access_key_id=${OPTARG} ;;
+        s) aws_secret_access_key=${OPTARG} ;;
+        r) aws_region=${OPTARG} ;;
+        h) usage
+           exit 0 ;;
+        *) usage
+           exit 1 ;;
+    esac
+done
+
+if [ -z "$aws_backup_bucket" ]; then
+  echo "$command: AWS backup bucket not set." >&2
   exit 1
 fi
 
-if [ -n "$AWS_SECRET_ACCESS_KEY" ]; then
-  echo "AWS_SECRET_ACCESS_KEY not set."
+if [ -z "$hostname" ]; then
+  echo "$command: Hostname not set." >&2
   exit 1
 fi
 
-if [ -n "$AWS_REGION" ]; then
-  region="us-east-1"
+if [ -z "$aws_access_key_id" ]; then
+  # shellcheck disable=SC2153
+  aws_access_key_id="$AWS_ACCESS_KEY_ID"
+  if [ -z "$aws_access_key_id" ]; then
+    echo "$command: AWS access key ID not set." >&2
+    exit 1
+  fi
 fi
 
-if [ -n "$AWS_BACKUP_BUCKET" ]; then
-  echo "AWS_BACKUP_BUCKET not set."
-  exit 1
+if [ -z "$aws_secret_access_key" ]; then
+  # shellcheck disable=SC2153
+  aws_secret_access_key="$AWS_SECRET_ACCESS_KEY"
+  if [ -z "$aws_secret_access_key" ]; then
+    echo "$command: AWS secret access key not set." >&2
+    exit 1
+  fi
 fi
 
-if [ -n "$HOSTNAME" ]; then
-  echo "HOSTNAME not set."
-  exit 1
+if [ -z "$aws_region" ]; then
+  # shellcheck disable=SC2153
+  aws_region="$AWS_REGION"
+  if [ -z "$aws_region" ]; then
+    aws_region="us-east-1"
+  fi
 fi
 
 content_type='application/x-compressed-tar'
 date=$(date -R)
 # shellcheck disable=SC2169,SC3028
-filename="$HOSTNAME-backup-$(date -Iseconds).tar.gz"
+filename="$hostname-backup-$(date -Iseconds).tar.gz"
 temp_dir="/tmp/openwrt-utils"
 backup_path_local="$temp_dir/$filename"
 backup_path_s3="$filename"
-backup_url="https://$AWS_BACKUP_BUCKET.s3.amazonaws.com/$backup_path_s3"
+backup_url="https://$aws_backup_bucket.s3.$aws_region.amazonaws.com/$backup_path_s3"
 
 trap 'rm $backup_path_local' EXIT
 
@@ -43,10 +76,10 @@ sysupgrade -b "$backup_path_local"
 
 echo "Uploading backup to S3 $backup_url."
 string_to_sign="PUT\n\n$content_type\n$date\n/$backup_path_s3"
-signature=$(echo -n "$string_to_sign" | openssl sha1 -hmac "$AWS_SECRET_ACCESS_KEY" -binary | base64)
+signature=$(echo -n "$string_to_sign" | openssl sha1 -hmac "$aws_secret_access_key" -binary | base64)
 curl -X PUT -T "$backup_path_local" \
-  -H "Host: $AWS_BACKUP_BUCKET.s3.$region.amazonaws.com" \
+  -H "Host: $aws_backup_bucket.s3.$aws_region.amazonaws.com" \
   -H "Date: $date" \
   -H "Content-Type: $content_type" \
-  -H "Authorization: AWS ${AWS_ACCESS_KEY_ID}:$signature" \
+  -H "Authorization: AWS ${aws_access_key_id}:$signature" \
   "$backup_url"
